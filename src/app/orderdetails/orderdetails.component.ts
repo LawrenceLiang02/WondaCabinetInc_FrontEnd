a2604b743528e2af9bb3d0bf17b6938424555aef
Vlad Popa <vpopa18@gmail.com>
2021-12-16 19:35:16 -0500

    Minor fixes after backend changes

--- src/app/orderdetails/orderdetails.component.ts
+++ src/app/orderdetails/orderdetails.component.ts
@@ -15,16 +15,16 @@ import { OrderServiceService } from '../view-orders/order-service.service';
        <strong>Status:</strong> {{order.orderStatus}}
       </div>
       <div >
-       <strong>Name:</strong> {{order.orderDetails.cabinetType}}
+       <strong>Name:</strong> {{order.cabinetType}}
       </div>
       <div >
-       <strong>Color:</strong> {{order.orderDetails.color}}
+       <strong>Color:</strong> {{order.color}}
       </div>
       <div >
-       <strong>Material:</strong> {{order.orderDetails.material}}
+       <strong>Material:</strong> {{order.material}}
       </div>
       <div >
-       <strong>Handle Type:</strong> {{order.orderDetails.handleType}}
+       <strong>Handle Type:</strong> {{order.handleType}}
       </div>
       <div>
         <embed type="application/pdf" scr="assets/pdf/210 Arctic Court, Oshawa - Island.pdf"/>
