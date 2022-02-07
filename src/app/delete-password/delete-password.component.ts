dc9cb578b5f54846e9480883ca81d01ff346eece
LawrenceLiang02 <lawrencekaiwei@gmail.com>
2022-02-06 22:16:48 -0500

    added parts of password validatoin

--- src/app/delete-password/delete-password.component.ts
+++ src/app/delete-password/delete-password.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { UserAuthService } from '../login/login.service';
 
 @Component({
   selector: 'app-delete-password',
@@ -7,7 +8,30 @@ import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
     <h2 mat-dialog-title>DELETE</h2>
     <mat-dialog-content class="mat-typography">
       <p>Please enter your password in order to continue.</p>
-      <input matInput [(ngModel)]="password" type="password">
+      <label for="password" style="font-size: 150%">Password</label>
+      <form #f="ngForm">
+        <input 
+        
+          type="password" 
+          ngModel class="form-control" 
+          id="password" 
+          name="password" 
+          style='max-width: 900px'
+          [(ngModel)]="form.password"
+          required
+          minlength="6"
+          #password="ngModel">
+      <div
+          class="alert alert-danger"
+          role="alert"
+          *ngIf="password.errors && f.submitted"
+        >
+          <div *ngIf="password.errors['required']">Password is required</div>
+          <div *ngIf="password.errors['minlength']">
+            Password must be at least 6 characters
+          </div>
+        </div>
+      </form>
     </mat-dialog-content>
     <mat-dialog-actions align="end">
       <button class="roundedbutton" mat-button mat-dialog-close>Cancel</button>
@@ -19,17 +43,42 @@ import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 })
 export class DeletePasswordComponent implements OnInit {
 
-  constructor( private dialog:MatDialog) { }
+  constructor( private dialog:MatDialog, private authService: UserAuthService) { }
   password: string;
+
+  form: any = {
+    username: null,
+    password: null
+  };
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
+
   ngOnInit(): void {
   }
 
   public onClick(){
+
     // const dialogConfig = new MatDialogConfig();
     // dialogConfig.disableClose = true;
     // dialogConfig.autoFocus = true;
     // dialogConfig.width = "60%";
     // this.dialog.open(DeletePasswordComponent);
+    const { username, password } = this.form;
+
+    this.authService.loginNoToken(username, password).subscribe(
+      data => {
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        // this.reloadPage();
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    );
 
   }
 }
